dbc965243d6b78f1649e1f5abdd22a6f5b236fcf
Sebastian <imanchesebastian@gmail.com>
2022-09-20 23:32:57 +0100

    allowed remote origin

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const PORT = process.env.NODE_ENV === "production" ? process.env.PORT : 3000;
 const corsOptions = {
   credentials: true,
   origin: [
+    "https://ipland-app.herokuapp.com",
     "http://localhost:5173",
-    "https://ipland-server.herokuapp.com/",
-    "https://ipland-app.herokuapp.com/",
+    "https://ipland-server.herokuapp.com",
   ],
 };
 
